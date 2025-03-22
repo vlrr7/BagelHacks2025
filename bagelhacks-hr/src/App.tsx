1fa32cbd458309b3eb9781d0fc774c2a8887021f
alexis <alex.is.here@icloud.com>
2025-03-22 19:15:24 -0400

    add frontend wireframe

--- bagelhacks-hr/src/App.tsx
+++ bagelhacks-hr/src/App.tsx
@@ -1,11 +1,26 @@
-import { useState } from "react";
 import "./App.css";
-import Header from "./components/Header";
+import About from "./pages/about.tsx";
+import Corporate from "./pages/corporate.tsx";
+import Jobseeker from "./pages/jobseeker.tsx";
+import Login from "./pages/login.tsx";
+import Profile from "./pages/profile.tsx";
+import Signup from "./pages/signup.tsx";
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <Header />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" Component={About} />
+        <Route path="/corporate" Component={Corporate} />
+        <Route path="/jobseeker" Component={Jobseeker} />
+        <Route path="/login" Component={Login} />
+        <Route path="/profile" Component={Profile} />
+        <Route path="/signup" Component={Signup} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
