75bf652fcc587d574b7ab802e56559773f6fe730
vlrr <comptedecode@gmail.com>
2025-03-23 00:00:55 -0400

    fixed environment errors

--- backend/database.py
+++ backend/database.py
@@ -4,14 +4,16 @@ from pymongo.server_api import ServerApi
 from dotenv import load_dotenv
 from flask import current_app
 
-uri = current_app.config["MONGO_MONGODB_URI"]
 
-client = MongoClient(uri, server_api=ServerApi('1'))
+def init_db():
+    uri = current_app.config["MONGO_MONGODB_URI"]
 
-try:
-    client.admin.command("ping")
-    print("Pinged your deployment. You successfully connected to MongoDB!")
-except Exception as e:
-    print("Error pinging MongoDB:", e)
+    client = MongoClient(uri, server_api=ServerApi('1'))
 
-db = client["bd"]
+    try:
+        client.admin.command("ping")
+        print("Pinged your deployment. You successfully connected to MongoDB!")
+    except Exception as e:
+        print("Error pinging MongoDB:", e)
+
+    return client["bd"]
