edcfd0be56c9fc537cb35fdf67e1036e45e2253f
rebecca <rebeccaabichahine@gmail.com>
2025-03-22 23:15:07 -0400

    parseFile.py

---
+++ backend/parseFile.py
@@ -0,0 +1,22 @@
+import PyPDF2
+
+def parse_pdf_to_text(file_path):
+    with open(file_path, 'rb') as fichier:
+        reader = PyPDF2.PdfReader(fichier)
+        num_pages = len(reader.pages)
+        text = []
+
+        for page in range(num_pages):
+            page_text = reader.pages[page].extract_text()
+            if page_text:
+                text.append(page_text)
+            else:
+                text.append(f"Page {page + 1} sans texte identifiable.")
+
+    text_complet = '\n'.join(text)
+    return text_complet
+
+if __name__ == "__main__":
+    file_path='/Users/rebecca/Library/CloudStorage/OneDrive-polymtlus/uni/uni/CVs/SE/Summer2025(finish autumn 2026)/RebeccaAbiChahine_CV_VersionFrancais.pdf'
+    text_extrait = parse_pdf_to_text(file_path)
+    print(text_extrait)
